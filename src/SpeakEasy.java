a4e801780d8b54afdfa03684a0b5aa33391a94c2
pawel <p4wos8000@gmail.com>
2020-10-21 23:46:38 +0200

    initial commit Basic layout added, contacts and groups added

--- src/SpeakEasy.java
+++ src/SpeakEasy.java
@@ -1,10 +1,20 @@
 import java.awt.*;
 import gui.*;
 
+import javax.swing.*;
+
 public class SpeakEasy
 {
     public static void main(String[] args)
     {
+        try
+        {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+        }
+        catch (Exception ex)
+        {
+            System.out.println("No GTKLookAndFeel, reseting to default...");
+        }
         EventQueue.invokeLater(() ->
         {
             SpeakEasyGUI frame = new SpeakEasyGUI("Speak Easy");
--- src/gui/SpeakEasyGUI.java
+++ src/gui/SpeakEasyGUI.java
@@ -1,13 +1,17 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.InputStream;
 
 public class SpeakEasyGUI extends JFrame
 {
     private JPanel mainPanel;
-    private JPanel groupsPanel;
-    private JPanel chatPanel;
-    private JPanel friendsPanel;
+    private JPanel groupsSegment;
+    private JPanel chatSegment;
+    private JPanel friendsSegment;
     private JPanel groupManagmentPanel;
     private JButton addGroupButton;
     private JTextField addGroupTextField;
@@ -15,7 +19,11 @@ public class SpeakEasyGUI extends JFrame
     private JPanel conversationPanel;
     private JPanel addFriendPanel;
     private JButton addFriendButton;
-    private JTextArea addFriendTextField;
+    private JTextField addFriendTextField;
+    private JPanel groupListPanel;
+    private JPanel groupList;
+    private JPanel friendListPanel;
+    private JPanel friendList;
 
     public SpeakEasyGUI(String title)
    {
@@ -23,5 +31,55 @@ public class SpeakEasyGUI extends JFrame
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        this.setContentPane(mainPanel);
        this.pack();
+       addGroupButton.addActionListener(new ActionListener()
+       {
+           @Override
+           public void actionPerformed(ActionEvent e)
+           {
+               String groupName = addGroupTextField.getText();
+               if (!groupName.equals(""))
+               {
+                   JButton newGroup = new JButton(groupName);
+                   newGroup.addActionListener(ev ->
+                   {
+                        groupList.remove(newGroup);
+                        groupList.revalidate();
+                        groupList.repaint();
+                   });
+                   GridBagConstraints gbc = new GridBagConstraints();
+                   gbc.gridx = 1;
+                   gbc.weightx = 1;
+                   gbc.fill = GridBagConstraints.HORIZONTAL;
+
+                   groupList.add(newGroup, gbc);
+                   groupList.revalidate();
+               }
+           }
+       });
+       addFriendButton.addActionListener(new ActionListener()
+       {
+           @Override
+           public void actionPerformed(ActionEvent e)
+           {
+                String friendName = addFriendTextField.getText();
+                if (!friendName.equals(""))
+                {
+                    JButton newFriend = new JButton(friendName);
+                    newFriend.addActionListener(ev ->
+                    {
+                        friendList.remove(newFriend);
+                        friendList.revalidate();
+                        friendList.repaint();
+                    });
+                    GridBagConstraints gbc = new GridBagConstraints();
+                    gbc.gridx = 1;
+                    gbc.weightx = 1;
+                    gbc.fill = GridBagConstraints.HORIZONTAL;
+
+                    friendList.add(newFriend, gbc);
+                    friendList.revalidate();
+                }
+           }
+       });
    }
 }
